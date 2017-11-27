5441bf08e0578891b72b6b68f25ea689e90b1cbc
Daniel Herzegh <danhe178@student.liu.se>
2017-11-27 16:20:07 +0100

    fixed variables

--- Body.py
+++ Body.py
@@ -15,11 +15,11 @@ class Body(object):
     def __init__(self, name, x_pos, y_pos, x_vel, y_vel, radius, mass):
         """Returns a Body"""
         self.name = name
-        self.x_pos= x_pos
+        self.x_pos = x_pos
         self.y_pos = y_pos
         self.x_vel = x_vel
         self.y_vel = y_vel
         self.radius = radius
         self.mass = mass
 
-   # def update_position(self):
+   #def update_position(self, x_pos, y_pos):
