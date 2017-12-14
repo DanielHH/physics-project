d2fc440f400e5916fc435ed0e2f22352b9a29f0c
Daniel Johnsson <danjo905@student.liu.se>
2017-12-14 14:43:04 +0100

    Added win and gameove

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ y_offset = 0
 center = ""
 monitor = ""
 gamePaused = False
+gameOver = False
+win = False
+isObjective = False
+b1 = ""
+b2 = ""
 
 def update(dt):
     global gamePaused
@@ -32,9 +37,17 @@ def update(dt):
         # We need to handle these events. Initially the only one you'll want to care
         # about is the QUIT event, because if you don't handle it, your game will crash
         # whenever someone tries to exit.
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos = pygame.mouse.get_pos()
+            if b1.collidepoint(pos):
+                sunSystem()
+                restart()
+            if b2.collidepoint(pos):
+                rockyraod()
+                restart()
         if event.type == KEYDOWN:
             if event.key == K_SPACE:
-                restart()
+                gamePaused = not gamePaused
         if event.type == QUIT:
             pygame.quit()  # Opposite of pygame.init
             sys.exit()  # Not including this line crashes the script on Windows. Possibly
@@ -59,33 +72,57 @@ def update(dt):
                 if isCollison(bodys[c[0]], bodys[c[1]]):
                     collison(bodys[c[0]], bodys[c[1]])
         for b in bodys:
-            velocityToPos(bodys[b])
+            velocityToPos(bodys[b], dt)
+    if isObjective:
+        objective()
 
 
 def restart():
     global gamePaused
-    bodys.clear()
+    global win
+    global gameOver
     #run gamemode function here:
-    sunSystem()
     gamePaused = False
+    win = False
+    gameOver = False
+
+def objective():
+    global gamePaused
+    global win
+    global gameOver
+    if "moon" in bodys:
+        b1 = bodys["moon"]
+        b2 = bodys[center]
+        dist = math.fabs(math.hypot(b1.x_pos - b2.x_pos, b1.y_pos - b2.y_pos))
+        if dist > 1000:
+            win = True
+            print win
+    else:
+        gameOver = True
+        gamePaused = True
 
 def sunSystem():
+    global isObjective
     global center
+    isObjective = True
+    bodys.clear()
     center = "sun"
-    rocket = Rocket("rocket", 100, 100, 0, 0, 10, 10, (100, 0, 100), True)
+    rocket = Rocket("rocket", 100, 100, 0, 0, 5, 10, (100, 0, 100), True)
     bodys[rocket.name] = rocket
-    addBody("earth", 400, 450, 0, -3, 60, 1000, (50, 100, 100), False)
-    addBody("moon", 300, 450, 0, -3.7, 20, 30, (100, 0, 0), True)
+    addBody("earth", 400, 450, 0, -8, 60, 1000, (50, 100, 100), False)
+    addBody("moon", 300, 450, 0, -10, 10, 10, (100, 0, 0), True)
     addBody("sun", 750, 450, 0, 0, 100, 10000, (100, 100, 0), False)
 
 def unElasticTest():
     global monitor
+    bodys.clear()
     monitor = "2"
     addBody("1", 0, 450, 4, 0, 60, 1000, (50, 100, 100), False)
     addBody("2", 1280, 450, -4, 0, 60, 1000, (50, 100, 100), False)
 
 def unElasticTestWithAngle():
     global monitor
+    bodys.clear()
     monitor = "2"
     addBody("1", 640, 0, 0, 4, 60, 1000, (50, 100, 100), False)
     addBody("2", 1280, 550, -4, 0, 60, 1000, (50, 100, 100), False)
@@ -93,6 +130,7 @@ def unElasticTestWithAngle():
 def bigBang():
     global monitor
     global center
+    bodys.clear()
     rocket = Rocket("rocket", 100, 100, 0, 0, 10, 10, (100, 100, 100), True)
     bodys[rocket.name] = rocket
     monitor = "rocket"
@@ -101,19 +139,42 @@ def bigBang():
         r1 = random.randint(0, 100)
         r2 = random.randint(0, 100)
         r3 = random.randint(0, 100)
-        x = random.randint(0, 2080)
-        y = random.randint(0, 1420)
+        x = random.randint(0, 1080)
+        y = random.randint(0, 720)
         p = random.randint(0, 1)
         x_vel = random.randint(0, 10)
         y_vel = random.randint(0, 20)
         r = random.randint(1, 20)
         m = random.randint(1, 100)
-        addBody(str(i), x, y, x_vel, y_vel, r, m, (r1, r2, r3), p)
+        addBody(str(i), x, y, x_vel, y_vel, r, m, (r1, r2, r3), True)
 
+def rockyraod():
+    global monitor
+    global center
+    center = ""
+    bodys.clear()
+    rocket = Rocket("rocket", 100, 100, 0, 0, 10, 100, (100, 100, 100), True)
+    bodys[rocket.name] = rocket
+    monitor = "rocket"
+    road = 0
+    for i in range(120):
+        r1 = random.randint(0, 100)
+        r2 = random.randint(0, 100)
+        r3 = random.randint(0, 100)
+        y = height/2
+        p = random.randint(0, 1)
+        x_vel = random.randint(0, 10)
+        y_vel = random.randint(0, 20)
+        r = random.randint(1, 10)
+        x = road + r
+        m = random.randint(1, 100)
+        addBody(str(i), x, y, 0, 0, r, m, (r1, r2, r3), True)
+        road += (r*2) +1
 
 def drawTest():
     global monitor
     global center
+    bodys.clear()
     rocket = Rocket("rocket", width + 100, height + 100, 0, 0, 10, 10, (100, 100, 100), True)
     bodys[rocket.name] = rocket
     monitor = "rocket"
@@ -124,15 +185,17 @@ def drawTest():
 def addBody(name, x_pos, y_pos, x_vel, y_vel, r, m, c, p):
     body = Body(name, x_pos, y_pos, x_vel, y_vel, r, m, c, p)
     bodys[body.name] = body
-def velocityToPos(b):
 
-    b.x_pos += b.x_vel
-    b.y_pos += b.y_vel
+def velocityToPos(b, dt):
+
+    b.x_pos += (b.x_vel * dt * 0.01)
+    b.y_pos += (b.y_vel * dt * 0.01)
 
 def isCollison(b1, b2):
     return b1.r+b2.r > distance(b1, b2)
 
 def collison(b1, b2):
+    global gameOver
     global gamePaused
     global center
     global monitor
@@ -150,6 +213,7 @@ def collison(b1, b2):
         b1.y_vel = -((b2.m * b2.y_vel) / b1.m)
     else:
         if b1.name == "rocket" or b2.name == "rocket":
+            gameOver = True
             gamePaused = True
         else:
             x_vel = ((b1.m * b1.x_vel) + (b2.m * b2.x_vel)) / (b2.m+b1.m)
@@ -223,15 +287,31 @@ def draw(screen):
         screen.blit(x_pos, (10, 100))
         screen.blit(y_pos, (10, 130))
     if gamePaused:
-        pausedgame = myfont.render("You crashed, press space to restart", False, (100, 100, 100))
+        drawPauseMenu(screen, myfont)
+    if win:
+        pausedgame = myfont.render("You win the moon is now lose in the universe!", False, (100, 100, 100))
+        screen.blit(pausedgame, (x_offset - 300, y_offset - 200))
+    if gameOver:
+        pausedgame = myfont.render("You lose! Chose new game mode", False, (100, 100, 100))
         screen.blit(pausedgame, (x_offset - 300, y_offset - 200))
     # Flip the display so that the things we drew actually show up.
     pygame.display.flip()
+
+def drawPauseMenu(screen, font):
+    global b1, b2
+
+    b1_text = font.render("Solar system", False, (0, 0, 0))
+    b1 = pygame.draw.rect(screen, (100, 100, 100), ((width/2) - 100, (height/2) -100, 200, 50))
+    screen.blit(b1_text, ((width/2) - 100 + 20, (height/2) - 100 + 20))
+    b2 = pygame.draw.rect(screen, (75, 75, 75), ((width/2) - 100, height/2, 200, 50))
+
+
 
 
 def runPyGame():
     global x_offset
     global y_offset
+    global gamePaused
     # Initialise PyGame.
     pygame.init()
     pygame.font.init()  # you have to call this at the start,
@@ -256,7 +336,7 @@ def runPyGame():
     # PyGame surfaces can be thought of as screen sections that you can draw onto.
     # You can also draw surfaces onto other surfaces, rotate surfaces, and transform surfaces.
 
-    restart()
+    gamePaused = True
     # Main game loop.
     dt = 1 / fps  # dt is the time since last frame.
     while True:  # Loop forever!
